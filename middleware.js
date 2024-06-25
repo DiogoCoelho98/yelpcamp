a0c17c533ff1c8e8b21363653381161b5b9c39e4
Diogo Coelho <diogo_coelho16@hotmail.com>
2024-06-25 20:08:13 +0100

    styles/review fixed

--- middleware.js
+++ middleware.js
@@ -9,7 +9,7 @@ const Review = require('./models/review');
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
-        req.session.returnTo = req.originalUrl; // add this line
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'You must be signed in first!');
         return res.redirect('/login');
     }
